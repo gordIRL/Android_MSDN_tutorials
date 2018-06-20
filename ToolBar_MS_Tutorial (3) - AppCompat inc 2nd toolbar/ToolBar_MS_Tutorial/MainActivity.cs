2de73d3cf486e4bf9023bbfd64853cbe065390d2
gordIRL <gord.leary@gmail.com>
2018-06-20 18:22:36 +0100

    another tutorial added

---
+++ ToolBar_MS_Tutorial (3) - AppCompat inc 2nd toolbar/ToolBar_MS_Tutorial/MainActivity.cs
@@ -0,0 +1,47 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+
+using Android.Support.V7.App;
+using Toolbar = Android.Support.V7.Widget.Toolbar;
+
+namespace ToolBar_MS_Tutorial
+{
+    [Activity(Label = "ToolBar_MS_Tutorial", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            var toolbar = FindViewById<Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            SupportActionBar.Title = "My AppCompat Toolbar";
+
+            var editToolbar = FindViewById<Toolbar>(Resource.Id.edit_toolbar);
+            editToolbar.Title = "Editing";
+            editToolbar.InflateMenu(Resource.Menu.edit_menus);
+            editToolbar.MenuItemClick += (sender, e) =>
+            {
+                Toast.MakeText(this, "Bottom toolbar tapped: " + e.Item.TitleFormatted, ToastLength.Short).Show();
+            };
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.top_menus, menu);
+            return base.OnCreateOptionsMenu(menu);
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            Toast.MakeText(this, "Action selected: " + item.TitleFormatted, ToastLength.Short).Show();
+            return base.OnOptionsItemSelected(item);
+        }
+    }
+}
+
